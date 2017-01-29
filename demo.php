6bb31773bf80c5d14a87492ba4074459ab955e63
Voidtek <vdacosta@voidtek.com>
2017-01-29 17:56:45 +0100

    V0.0 - Beta Version (#1)
    * Creation of the first class.
    
    * Code sniffer + new dependencies
    
    * Add phpcs into travis.
    
    * Add buttons on the README.
    
    * Clean staff.
    
    * Load Data + caching + logs.
    
    * Fix Code sniffer.

--- demo.php
+++ demo.php
@@ -1,10 +1,30 @@
-<?php 
+<?php
+/**
+ * File demo.php
+ *
+ * PHP Version 5
+ *
+ * @category PHP
+ * @package  WindguruIO
+ * @author   Voidtek <vdacosta@voidtek.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/voidtek/windguru.io
+ */
 
-require_once './vendor/autoload.php';
+require_once __DIR__ . "/vendor/autoload.php";
 
-use GuzzleHttp\Psr7\Request;
+use voidtek\WindguruIO\WindguruAPI;
 
-$request = new Request('PUT', 'http://httpbin.org/put');
-$response = $client->send($request, ['timeout' => 2]);
+if (PHP_SAPI === 'cli') {
+    $idspot = $argv[1];
+} else {
+    $idspot = $_GET['idspot'];
+}
 
+if (empty($idspot)) {
+    return;
+}
 
+$windguruAPI = new WindguruAPI();
+$windguruAPI->setSpot($idspot);
+$windguruAPI->getData();
